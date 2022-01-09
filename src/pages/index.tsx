b8c15672e79cd8168c96ea02cdc543899c0073fb
unract-kohiki <developerjunki@gmail.com>
2022-01-09 22:45:54 +0900

    fix:#17
    styleディレクトリをsrcの中に移動

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Heading, Page } from "@shopify/polaris";
 const Index = () => (
   <Page>
     <Heading>Shopify app with Node and React 🎉</Heading>
+    <p className="text-red-600">こんにちは、tailwindが効いていれば赤色に表示されます</p>
   </Page>
 );
 
