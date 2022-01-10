f3e34b20cc3ec007c41923b3d122bb9e29f6fc6d
unract-kohiki <developerjunki@gmail.com>
2022-01-10 21:33:56 +0900

    update:#3
    請求画面へのリダイレクトの際のちらつきを解消

---
+++ src/lib/querys/get-subscription-status.ts
@@ -0,0 +1,32 @@
+import "isomorphic-fetch";
+import ApolloClient, { gql } from "apollo-boost";
+
+// サブスクリプションのstatusを取得するためのクエリ
+const GET_SUBSCRIPTION_STATUS = gql`
+  query {
+    currentAppInstallation {
+      activeSubscriptions {
+        createdAt
+        name
+        status
+        test
+        trialDays
+      }
+    }
+  }
+`;
+
+type GetSubscriptionStatus = (client: ApolloClient<any>) => Promise<boolean>;
+
+export const getSubscriptionStatus: GetSubscriptionStatus = async (client) => {
+  const isSubscriptionStatusActive = await client
+    .query({ query: GET_SUBSCRIPTION_STATUS })
+    .then((response) => {
+      return response.data.currentAppInstallation?.activeSubscriptions[0]
+        ?.status == "ACTIVE"
+        ? true
+        : false;
+    });
+
+  return isSubscriptionStatusActive;
+};
