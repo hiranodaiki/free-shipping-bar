e487556b82547e5c7c77472d7e1eac7d24a9a22a
unreact-saito <saito.eiji@unreact.jp>
2022-01-16 16:09:29 +0900

    fix tailwind.config.js に maxWidth の値を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend:{
+      maxWidth: {
+        xs: '320px',
+        sm: '384px',
+        md: '448px',
+        lg: '512px',
+        xl: '576px',
+        "2xl": '672px',
+        "3xl": '768px',
+        "4xl": '896px',
+        "5xl": '1024px',
+        "6xl": '1152px',
+        "7xl": '1280px',
+      },
       spacing: {
         px: '1px',
         0: '0px',
